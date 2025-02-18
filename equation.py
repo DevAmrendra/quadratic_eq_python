3f58e0d626924dbaaca551eb00d42a8ce62b3534
Amrendra Singh <113918515+DevAmrendra@users.noreply.github.com>
2025-02-18 18:09:08 +0530

    Update equation.py
    Updated the code to prevent wrong division, added braces.

--- equation.py
+++ equation.py
@@ -3,15 +3,15 @@ import math, cmath
 def solve_quadratic(a, b, c):
     discriminant = (b**2 - 4*a*c)
     if discriminant > 0:
-       root1 = (-b + math.sqrt(discriminant))/2*a
-       root2 = (-b - math.sqrt(discriminant))/2*a
+       root1 = (-b + math.sqrt(discriminant)) / (2 * a)
+       root2 = (-b - math.sqrt(discriminant)) / (2 * a)
        return root1, root2
     elif discriminant == 0:
-        root = (-b/2*a)
+        root = -b / (2 * a)
         return root
     else:
-        root1 = (-b + cmath.sqrt(discriminant))/2*a
-        root2 = (-b - cmath.sqrt(discriminant))/2*a
+        root1 = (-b + cmath.sqrt(discriminant)) / (2 * a)
+        root2 = (-b - cmath.sqrt(discriminant)) / (2 * a)
         return root1, root2
 
 a = int(input("Enter Coefficient Of x^2: "))
